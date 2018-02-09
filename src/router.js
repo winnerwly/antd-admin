507edd5148da4ce4beb80f1eeead1249fa009178
winnerwly <975080391@qq.com>
2018-02-09 10:21:44 +0800

    ..

--- src/router.js
+++ src/router.js
@@ -28,40 +28,6 @@ const Routers = function ({ history, app }) {
       path: '/login',
       models: () => [import('./models/login')],
       component: () => import('./routes/login/'),
-    }, {
-      path: '/request',
-      component: () => import('./routes/request/'),
-    }, {
-      path: '/UIElement/iconfont',
-      component: () => import('./routes/UIElement/iconfont/'),
-    }, {
-      path: '/UIElement/search',
-      component: () => import('./routes/UIElement/search/'),
-    }, {
-      path: '/UIElement/dropOption',
-      component: () => import('./routes/UIElement/dropOption/'),
-    }, {
-      path: '/UIElement/layer',
-      component: () => import('./routes/UIElement/layer/'),
-    }, {
-      path: '/UIElement/dataTable',
-      component: () => import('./routes/UIElement/dataTable/'),
-    }, {
-      path: '/UIElement/editor',
-      component: () => import('./routes/UIElement/editor/'),
-    }, {
-      path: '/chart/ECharts',
-      component: () => import('./routes/chart/ECharts/'),
-    }, {
-      path: '/chart/highCharts',
-      component: () => import('./routes/chart/highCharts/'),
-    }, {
-      path: '/chart/Recharts',
-      component: () => import('./routes/chart/Recharts/'),
-    }, {
-      path: '/post',
-      models: () => [import('./models/post')],
-      component: () => import('./routes/post/'),
     },
   ]
 
